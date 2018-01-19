13cd2aff5282db2533507486a78029a1ffdea65d
awonderfullife <cooper.yi@outlook.com>
2018-01-19 01:14:39 -0800

    optimization and show

--- stylize.py
+++ stylize.py
@@ -73,7 +73,7 @@ def stylize(
 			style_pre = np.array([VGG_model.preprocess(styles[i], vgg_mean_pixel)])
 			for layer in STYLE_LAYERS:
 				features = net[layer].eval(feed_dict={image: style_pre})
-				features = np.reshape(features, (-1, features.shape[3]))
+				features = np.reshape(features, (-1, features.content_shape[3]))
 				gram = np.matmul(features.T, features)/features.size   # due to mutilayer, we need normalize to make combination become successfull
 				style_features[i][layer] = gram
 
@@ -82,13 +82,13 @@ def stylize(
 	# make stylized iamge using backpropogation
 	with tf.Graph().as_default():
 		if initial is None:
-			noise = np.random.normal(size=shape, scale=np.std(content)*0.1)
-			initial = tf.random_normal(shape)*0.256
+			noise = np.random.normal(size=content_shape, scale=np.std(content)*0.1)
+			initial = tf.random_normal(content_shape)*0.256
 		else:
 			initial = np.array([VGG_model.preprocess(initial, vgg_mean_pixel)])
 			initial = initial.astype('float32')
-			noise = np.random.normal(size=shape, scale=np.std(content)*0.1)
-			initial = initial*initial_content_nosie_coeff + (tf.random_normal(shape)*0.256)*(1.0-initial_content_nosie_coeff)
+			noise = np.random.normal(size=content_shape, scale=np.std(content)*0.1)
+			initial = initial*initial_content_nosie_coeff + (tf.random_normal(content_shape)*0.256)*(1.0-initial_content_nosie_coeff)
 		image = tf.Variable(initial)
 		net = VGG_model.net_preloaded(vgg_weights, image, pooling)
 
@@ -118,11 +118,86 @@ def stylize(
 				style_losses.append(style_layers_weights[style_layer]*2*tf.nn.l2_loss(gram-style_gram)/style_gram.size)
 			style_loss += style_weight*style_blend_weights[i]*reduce(tf.add, style_losses)
 
-		
+		# total variation denoising
+		tv_y_size = _tensor_size(image[:,1:,:,:])
+		tv_x_size = _tensor_size(image[:,:,1:,:])
+		tv_loss = tv_weight * 2 * ((tf.nn.l2_loss(image[:,1:,:,:] - image[:,:content_shape[1]-2,:,:]) / tv_y_size)
+			+ (tf.nn.l2_loss(image[:,:,1:,:] - image[:,:,:content_shape[2]-1,:]) / tv_x_size))
 
+		# overall loss 
+		loss = content_loss + style_loss + tv_loss
 
+		# optimizer setup
+		train_step = tf.tain.AdaOptimizer(learning_rate, beta1, beta2, epsilon).minimize(loss)
 
+		def print_progress():
+			stderr.write('content loss: %g\n' % content_loss.eval())
+			stderr.write('  style loss: %g\n' % style_loss.eval())
+			stderr.write('     tv loss: %g\n' % tv_loss.eval())
+			stderr.write('  total loss: %g\n' % loss.eval())
 
+		# optimization
+		best_loss = float('inf')
+		best = None
+		with tf.Session() as sess:
+			sess.run(tf.global_variables_initializer())
+			stderr.write('Optimization started...\n')
+			if (print_iterations and print_iterations != 0):
+				print_progress()
+			for i in xrange(iterations):
+				stderr.write('Iteration %4d/%4d' % (i+1, iterations))
+				train_step.run()
+
+				last_step = (i == iterations-1)
+				if last_step or (print_iterations and i%print_iterations==0):
+					print_progress()
+
+				if (checkpoint_iterations and i%checkpoint_iterations==0) or last_step:
+					this_loss = loss.eval()
+					if this_loss < best_loss:
+						best_loss = this_loss
+						best = image.eval()
+
+					img_out = vgg.unprocess(best.reshape(content_shape[1:]), vgg_mean_pixel)
+
+					if preserve_colors and preserve_colors == True:
+						original_image = np.clip(content, 0, 255)  # the value out of [0, 255] will be assign to 0 or 255
+						styled_image = np.clip(img_out, 0, 255)
+
+						# Luminosity transfer:
+						# step 1: Convert stylized RGB->grayscale accoriding to Rec.601 luma (0.299, 0.587, 0.114)
+						styled_grayscale = rgb2gray(styled_image)
+						styled_grayscale_rgb = gray2rgb(styled_grayscale)
+
+						# step 2: Convert stylized grayscale into YUV (YCbCr)
+						styled_grayscale_yuv = np.array(Image.fromarray(original_image.astype(np.uint8)).convert('YCbCr'))
+
+						# step 3: Convert original image into YUV (YCbCr)
+						original_yuv = np.array(Image.fromarray(original_image.astype(np.uint8)).convert('YCbCr'))
+
+						# step 4: Recombine (stylizedYUV.Y, originalYUV.U, originalYUV.V)
+						w, h, _ = original_image.shape 
+						combined_yuv = np.empty((w,h,3), dtype=np.uint8)
+						combined_yuv[..., 0] = styled_grayscale_yuv[..., 0]
+						combined_yuv[..., 1] = original_yuv[..., 1]
+						combined_yuv[..., 2] = original_yuv[..., 2]
+
+						# step 5: Convert recombined image from YUV back to RGB
+						img_out = np.array(Image.fromarray(combined_yuv, 'YCbCr').convert('RGB'))		
+
+					yield (
+						(None, if last_step else i),
+						img_out
+						)
+
+def rgb2gray(rgb):
+	return np.dot(rgb[..., 3], [0.299, 0.587, 0.114])  # Dot product of two arrays. For 2-D arrays it is equivalent to matrix multiplication
+
+def gray2rgb(gray):
+	w, h = gray.shape
+	rgb = np.empty((w, h, 3), dtype=np.float32)
+	rgb[:, :, 2] = rgb[:, :, 1] = rgb[:, :, 0] = gray
+	return rgb 
 
 
 
---
+++ test.py
@@ -0,0 +1,10 @@
+from sys import stderr
+
+var1 = 0
+var2 = None
+
+if var1:
+	stderr.write('Nihao')
+
+if var2:
+	stderr.write('Nihao2')
